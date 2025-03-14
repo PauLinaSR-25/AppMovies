4da185a672b7e8645af6ffc542edd4bd30364178
Paulina Sanchez <paulina.sanchezs@elektra.com.mx>
2025-03-14 16:53:23 -0600

    Initial Commit

---
+++ TopMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopMovies
+//
+//  Created by Paulina Sanchez Salazar on 14/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TopMoviesTests/TopMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TopMoviesTests.swift
+//  TopMoviesTests
+//
+//  Created by Paulina Sanchez Salazar on 14/03/25.
+//
+
+import Testing
+@testable import TopMovies
+
+struct TopMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
